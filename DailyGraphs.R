d69b5d4cdb97a0927926df407079f634d5993c2f
rivatal <riva.tropp@mail.yu.edu>
2016-10-07 12:53:49 -0400

    graphics in folder.

--- DailyGraphs.R
+++ DailyGraphs.R
@@ -20,7 +20,7 @@ g <- ggplot(averagedays, aes(x = day_of_week, y=entries, fill = entries)) +
   geom_bar(stat="identity") +
   scale_y_continuous(label=comma) +
   xlab("Time") + ylab("Entries") + ggtitle("Most Popular Days") 
-ggsave(filename = "daypopularity.png", plot=g)
+ggsave(filename = "graphics/daypopularity.png", plot=g)
 
 ###########################################################################################
 #Mean with and without weekends
@@ -48,21 +48,21 @@ g <- ggplot(data = popular_times, aes(x = time_bucket, y = entries, fill = time_
   scale_y_continuous(label = comma)+
   scale_fill_brewer(direction = -1) +
   xlab("Time") + ylab("Entries") + ggtitle("Average entries by time of day (over the entire system)") 
-ggsave("avg_entries.png", plot=g)
+ggsave("graphics/avg_entries.png", plot=g)
 
 h <- ggplot(data = popular_times, aes(x = time_bucket, y = exits, fill = time_bucket)) +
   geom_bar(stat = "identity") +
   scale_y_continuous(label = comma)+
   scale_fill_brewer(direction = -1) +
   xlab("Time") + ylab("Exits") + ggtitle("Average exits by time of day (over the entire system)") 
-ggsave("avg_exits.png", plot=h)
+ggsave("graphics/avg_exits.png", plot=h)
 
 i <- ggplot(data = popular_times, aes(x = time_bucket, y = entries/exits, fill = time_bucket)) +
   geom_bar(stat = "identity") +
   scale_y_continuous(label = comma)+
   scale_fill_brewer() +
   xlab("Time") + ylab("Ratio of Entries to Exits") + ggtitle("Average entries/exits by time of day") 
-ggsave("entries_exits_ratio.png", plot=i)
+ggsave("graphics/entries_exits_ratio.png", plot=i)
 
 ###########################################################################################
 #Day by day overview of entries and exits
@@ -84,43 +84,84 @@ for(i in 1:12){
   }
   g<- ggplot(data = month, aes(x = day, y=entries, group=year)) + geom_line(aes(color=year)) +
     scale_y_continuous(label = comma) + scale_x_discrete()
-  ggsave(paste0(paste0("all_", i), ".png"), plot=g)
+  ggsave(paste0("graphics/", paste0(paste0("all_", i), ".png")), plot=g)
 }
 
 #Make graphs of every month of every year.
-for(j in 2015:2016){
-  for(i in 1:12){
-    if(i < 10){
-        x <- days %>% filter(month == (paste0("0", i)), year == j)
-    }else{
-      x <-days %>% filter(month == i) %>% filter(year == j)
-    }
-    print(i)
-    print(j)
-    View(x)
-    if(!is.null(x)){
-      g <- ggplot(data = x, aes(x = day, y=entries, group=1)) + geom_line(color = "steelblue") +
-      scale_y_continuous(label = comma) + ggtitle(i)
-      ggsave(filename = paste0(paste0(paste0("plot", i), j), ".png"), plot = g)
-    }
-  }
-}
-j <- 2014
+#for(j in 2015:2016){
+#  for(i in 1:12){
+#    if(i < 10){
+#        x <- days %>% filter(month == (paste0("0", i)), year == j)
+#    }else{
+#      x <-days %>% filter(month == i) %>% filter(year == j)
+#    }
+#    print(i)
+#    print(j)
+#    if(!is.null(x)){
+#      g <- ggplot(data = x, aes(x = day, y=entries, group=1)) + geom_line(color = "steelblue") +
+#      scale_y_continuous(label = comma) + ggtitle(i)
+#      ggsave(filename = paste0(paste0(paste0("plot", i), j), ".png"), plot = g)
+#    }
+#  }
+#}
+#j <- 2014
 #months <- days %>% filter(year == 2014) %>% group_by(month) %>% summarise()
 #Have to run 2014 separately for lack of months
-for(i in 10:12){
-  print(i)
-  if(i < 10){
-    x <- days %>% filter(month == (paste0("0", i)), year == j)
-  }else{
-    x <-days %>% filter(month == i) %>% filter(year == j)
-  }
-  print(i)
-  print(j)
-  View(x)
-  if(!is.null(x)){
-    g <- ggplot(data = x, aes(x = date, y=entries, group=1)) + geom_line(color = "steelblue") +
-      scale_y_continuous(label = comma) + ggtitle(i)
-    ggsave(filename = paste0(paste0(paste0("plot", i), j), ".png"), plot = g)
-  }
-}
+#for(i in 10:12){
+#  print(i)
+#  if(i < 10){
+#    x <- days %>% filter(month == (paste0("0", i)), year == j)
+#  }else{
+#    x <-days %>% filter(month == i) %>% filter(year == j)
+#  }
+#  print(i)
+#  print(j)
+#  View(x)
+#  if(!is.null(x)){
+#    g <- ggplot(data = x, aes(x = date, y=entries, group=1)) + geom_line(color = "steelblue") +
+#      scale_y_continuous(label = comma) + ggtitle(i)
+#    ggsave(filename = paste0(paste0(paste0("plot", i), j), ".png"), plot = g)
+#  }
+#}
+
+####################################################################################
+#Testing my nums
+####################################################################################
+days$date <- as.Date(days$date, format="%m/%d/%Y")
+
+myfunc <- function(x,y){days[days$date >= x & days$date <= y,]}
+DATE1 <- as.Date("06/27/2015", format="%m/%d/%Y")
+DATE2 <- as.Date("07/03/2015", format="%m/%d/%Y")
+test <- myfunc(DATE1,DATE2)
+test <- test[c(1,5)]
+names(test) <- c("date", "r_entries")
+
+july <- read.csv("turnstile_data/julynums.csv", header = FALSE)
+names(july) <- c("date", "awk_entries")
+july$date <- as.Date(july$date, format="%m/%d/%Y")
+
+july <- full_join(july, test)
+july <- july %>% mutate(diff = r_entries - awk_entries) %>% mutate(absdif = abs(diff))
+
+#The difference, at least for this particular subset, seems quite high
+#and not always positive or always negative, as we'd expect if the problem is merely
+#joining on station names. But to be sure, let's create a load_subway_trips that 
+#calculates days based on station names and see how much closer it is.
+source("station_blind_load_subway_trips.R")
+station_entries_exits <- read.csv("station_entries_exits.csv")
+colnames(station_entries_exits)[4] <- "entries"
+colnames(station_entries_exits)[5] <- "exits"
+station_entries_exits <- station_entries_exits %>% mutate(year = substring(station_entries_exits$date, 7,11))
+station_entries_exits <- station_entries_exits %>% mutate(month = substring(station_entries_exits$date, 1, 2))
+station_entries_exits <- station_entries_exits %>% mutate(day = substring(date, 4,5))
+
+#All exits/entries per day across the system.
+days <- station_entries_exits[complete.cases(station_entries_exits),] %>% group_by(date, year, month, day) %>% summarise(sum(as.numeric(entries)), sum(exits))
+names(days) <- c("date", "year", "month", "day", "entries", "exits")
+days$date <- as.Date(days$date, format="%m/%d/%Y")
+test <- myfunc(DATE1,DATE2)
+test <- test[c(1,5)]
+names(test) <- c("date", "r_station_entries")
+july <- full_join(july, test)
+july <- mutate(july, awkdiff = awk_entries - r_station_entries)
+july <- mutate(july, rvsrstation = r_entries - r_station_entries)
---
+++ turnstile_data/test.awk
@@ -0,0 +1,47 @@
+#sort -k 7,7 turnstile_150704.txt --field-separator=, > sortedts.txt
+BEGIN{
+    FS = ","    
+    file = "sortedts.txt"
+    ncount = 0
+    fsize = 0
+    if(ARGV[1] == "n" || ARGV[1] == "negativecounters"){
+	while((getline < file) > 0){
+	    curr = $1 "," $2 "," $3 "," $4 "," $8 "," $7 "," $10
+	    if($1 == ca && $2 == unit && scp == $3 && station == $4){
+		diff = $10-lastentries
+		if(diff < 0){
+		    print lastall "\n" curr > "bad.txt"
+		    ncount++
+		}   
+	    }
+	    ca = $1
+	    unit = $2
+	    scp = $3
+	    station = $4
+	    lastentries = $10
+	    lastall = $1 "," $2 "," $3 "," $4 "," $8 "," $7 "," $10
+	    fsize++
+	}
+	print "Found " ncount " negative records out of " fsize " total records. (" ncount/fsize*100 " percent)." 
+    }
+    else{
+	OFS = ","
+	while((getline < file) > 0){
+	    curr = $1 "," $2 "," $3 "," $4 "," $8 "," $7 "," $10
+	    diff = $10 - lastentries
+	    if($1 == ca && $2 == unit && scp == $3 && station == $4 && diff > 0 && diff < 100000){
+		key = $7 #The date
+		dict[key] += diff
+	    }
+	    ca = $1
+	    unit = $2
+	    scp = $3
+	    station = $4
+	    lastentries = $10
+	    lastall = $1 "," $2 "," $3 "," $4 "," $8 "," $7 "," $10
+	}	    
+	for(x in dict){
+	    print x, dict[x]
+	}
+    }
+}
